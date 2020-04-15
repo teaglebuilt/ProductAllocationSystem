cecc5b81a83f7676b4ed01f976d1d631beaab4ef
Dillan Teagle <dillan.teagle.va@gmail.com>
2020-04-15 08:58:34 -0500

    :recycle: refactor Batch class

--- test_batches.py
+++ test_batches.py
@@ -28,4 +28,9 @@ def test_matching_in_allocation():
     line = OrderLine('R-32', 'Wood', qty=3)
     batch.can_allocate(line) is False
 
-    
+
+def test_allocation_is_idempotent():
+    batch, line = make_batch_and_line("ANGULAR-DESK", 20, 2)
+    batch.allocate(line)
+    batch.allocate(line)
+    assert batch.available_quantity == 18
